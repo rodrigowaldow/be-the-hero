0982b41b162f5535648c0a5724debbcf1360b86a
Rodrigo Waldow <waldowrodrigo@gmial.com>
2020-03-26 22:30:09 -0300

    [ADD] Profile page

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi'
 
 import './styles.css';
@@ -17,10 +17,10 @@ export default function Logon() {
 
 							<input type="text" placeholder="Sua ID"/>
 							<button className="button" type="submit">Entrar</button>
-
-							<a href="/register">
+							<Link className="back-link" to="/register">
 								<FiLogIn size={16} color="#E02041"/>
-								Não tenho cadastro</a>
+								Não tenho cadastro
+							</Link>
 						</form>
           </section>
 
