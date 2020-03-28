8e80caca6469eeea8778d5ffdc63eec7c03d552e
Rodrigo Waldow <waldowrodrigo@gmial.com>
2020-03-28 16:20:59 -0300

    [ADD] conectt to backend

--- mobile/App.js
+++ mobile/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-
 import Routes from './src/routes';
+
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
 
 export default function App() {
   return (
--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { Text, View, Image, TouchableOpacity, Linking } from 'react-native';
 
 import * as  MailComposer from 'expo-mail-composer';
@@ -10,7 +10,11 @@ import styles from './styles';
 
 export default function Incidents() {
 	const navigation = useNavigation();
-	const message = 'Olá ONG, estou entrando em contato pois gostaria de ajudar no caso "Cadelinha atropelada" com o valor de R$120;'
+
+	const route = useRoute();
+	const incident = route.params.incident;
+
+	const message = `Olá ${incident.name}, estou entrando em contato pois gostaria de ajudar no caso "${incident.title}" com o valor de ${Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL'}).format(incident.value)};`
 
 	function navigateBack(e) {
 		navigation.goBack();
@@ -18,19 +22,19 @@ export default function Incidents() {
 
 	function sendEmail() {
 		MailComposer.composeAsync({
-			subject: 'Herói do caso: Teste',
-			recipients: ['waldowrodrigo@gmail.com'],
+			subject: `Herói do caso: ${incident.title}`,
+			recipients: [incident.email],
 			body: message
 		});
 	}
 
 	function sendWhatsapp() {
-		Linking.openURL(`whatsapp://send?phone=5551996512190&text=${message}`);
+		Linking.openURL(`whatsapp://send?phone=${incident.whatsapp}&text=${message}`);
 	}
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.header}>
+	return (
+		<View style={styles.container}>
+			<View style={styles.header}>
 					<Image source={logoImg} />
 					
 					<TouchableOpacity
@@ -43,13 +47,17 @@ export default function Incidents() {
 
 			<View style={styles.incident}>
 				<Text style={[styles.incidentProperty, { marginTop: 0 }]}>ONG:</Text>
-				<Text style={styles.incidentValue}>APAD</Text>
+				<Text style={styles.incidentValue}>{incident.name} de {incident.city}/{incident.uf}</Text>
 
 				<Text style={styles.incidentProperty}>CASO:</Text>
-				<Text style={styles.incidentValue}>Teste</Text>
+				<Text style={styles.incidentValue}>{incident.description}</Text>
 
 				<Text style={styles.incidentProperty}>VALOR:</Text>
-				<Text style={styles.incidentValue}>R$ 120,00</Text>
+				<Text style={styles.incidentValue}>
+					{Intl.NumberFormat('pt-BR', {
+						style: 'currency', 
+						currency: 'BRL'
+					}).format(incident.value)}</Text>
 			</View>
 
 			<View style={styles.contactBox}>
@@ -67,7 +75,7 @@ export default function Incidents() {
 					</TouchableOpacity>
 				</View>
 			</View>
-    </View>
-  );
+		</View>
+	);
 }
 
