f9d5442bd317136c39e3c8d499d73774cb3022a0
Rodrigo Waldow <waldowrodrigo@gmial.com>
2020-03-27 22:51:12 -0300

    [ADD] logout ong

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi'
 
 import api from '../../services/api';
@@ -9,8 +9,11 @@ import logoImg from '../../assets/logo.svg';
 
 export default function Profile() {
 	const [incidents, setIncidents] = useState([]);
+
 	const ongName = localStorage.getItem('ongName');
 	const ongId = localStorage.getItem('ongId');
+
+	const history = useHistory();
 
 	useEffect(() => {
 		api.get('profile', {
@@ -37,6 +40,11 @@ export default function Profile() {
 		}
 	}
 
+	function handleLogout() {
+		localStorage.clear();
+		history.push('/');
+	}
+
 	return (
 			<div className="profile-container">
 					<header>
@@ -44,7 +52,7 @@ export default function Profile() {
 							<span>Bem vinda, {ongName}</span>
 							
 							<Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-							<button type="button">
+							<button onClick={handleLogout} type="button">
 									<FiPower size={18} color="#E02041"/>
 							</button>
 					</header>
