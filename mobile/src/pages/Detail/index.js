6d8244b65af623e1fdfcdc6dd8bcce06c4c881cc
Rodrigo Waldow <waldowrodrigo@gmial.com>
2020-03-28 00:58:29 -0300

    [ADD] Create incidents list mobile

---
+++ mobile/src/pages/Detail/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function Detail() {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+}
+
